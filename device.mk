199b3a0171a6255fb3d9d425389e7ebd27fc6e2c
fadlyas07 <fadlyardhians@gmail.com>
2020-07-07 15:53:16 +0700

    rolex: Add Firmware from MIUI V10.2.3.0.NCCMIXM
    firmware: https://github.com/XiaomiFirmwareUpdater/firmware_xiaomi_rolex/releases/download/stable-19.08.2019/fw_rolex_miui_HM4AGlobal_V10.2.3.0.NCCMIXM_69d07208fd_7.1.zip
    releasetool.py: https://github.com/muralivijay/android_device_xiaomi_rolex/raw/lineage-16.0/releasetools.py
    
    Signed-off-by: fadlyas07 <fadlyardhians@gmail.com>
    Change-Id: I995e9eaf8488660b9bd32d23fbca656f20964088

--- device.mk
+++ device.mk
@@ -143,6 +143,23 @@ PRODUCT_PACKAGES += \
     android.hardware.drm@1.0-service \
     android.hardware.drm@1.2-service.clearkey
 
+# Firmware target releasetool
+TARGET_RELEASETOOLS_EXTENSIONS := device/xiaomi/rolex
+
+# Firmware target PATH
+RODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/firmware/adspso.bin:install/firmware-update/adspso.bin \
+    $(LOCAL_PATH)/firmware/cmnlib64.mbn:install/firmware-update/cmnlib64.mbn \
+    $(LOCAL_PATH)/firmware/cmnlib.mbn:install/firmware-update/cmnlib.mbn \
+    $(LOCAL_PATH)/firmware/devcfg.mbn:install/firmware-update/devcfg.mbn \
+    $(LOCAL_PATH)/firmware/emmc_appsboot.mbn:install/firmware-update/emmc_appsboot.mbn \
+    $(LOCAL_PATH)/firmware/keymaster.mbn:install/firmware-update/keymaster.mbn \
+    $(LOCAL_PATH)/firmware/lksecapp.mbn:install/firmware-update/lksecapp.mbn \
+    $(LOCAL_PATH)/firmware/NON-HLOS.bin:install/firmware-update/NON-HLOS.bin \
+    $(LOCAL_PATH)/firmware/rpm.mbn:install/firmware-update/rpm.mbn \
+    $(LOCAL_PATH)/firmware/sbl1.mbn:install/firmware-update/sbl1.mbn \
+    $(LOCAL_PATH)/firmware/tz.mbn:install/firmware-update/tz.mbn
+
 # FM
 PRODUCT_PACKAGES += \
     FMRadio \
