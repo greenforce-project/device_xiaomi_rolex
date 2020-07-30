061cdff4c3bc4e71a4296efbadd556e4da1543b6
fadlyas07 <fadlyardhians@gmail.com>
2020-07-30 07:42:53 +0700

    rolex: vendor_prop: fixing my bad -_
    * partial revert from commits cb50781341c0e651690c090dc9d7b9d987fa2e3d.
    
    Signed-off-by: fadlyas07 <fadlyardhians@gmail.com>
    Change-Id: I90fe3b67c40ef9c51e1d6f31236d4e30b960af1e

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -112,6 +112,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
     persist.hwc.mdpcomp.enable=true \
     vendor.display.disable_rotator_split=1 \
     vendor.display.disable_skip_validate=1 \
+    vendor.display.perf_hint_window=50 \
     vendor.gralloc.enable_fb_ubwc=1 \
     sys.display-size=1280x720
 
