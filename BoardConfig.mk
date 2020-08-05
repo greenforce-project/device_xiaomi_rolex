7e5eaa56a4060a9a251a4cfca2627640b040589a
fadlyas07 <fadlyardhians@gmail.com>
2020-08-05 22:47:45 +0000

    Revert "rolex: ignore SELinux Neverallows"
    This reverts commit fab0f105019c2856ad5fbc535699c81320bbcb08.
    
    Change-Id: Ibc65c7817def431154938210e36a43e75e236291

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -188,11 +188,6 @@ VENDOR_SECURITY_PATCH := 2020-03-05
 include device/qcom/sepolicy-legacy-um/sepolicy.mk
 BOARD_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy
 
-# We modify several neverallows, so let the build proceed
-ifneq ($(TARGET_BUILD_VARIANT),user)
-SELINUX_IGNORE_NEVERALLOWS := true
-endif
-
 # Treble
 BOARD_PROPERTY_OVERRIDES_SPLIT_ENABLED := true
 PRODUCT_FULL_TREBLE_OVERRIDE := true
